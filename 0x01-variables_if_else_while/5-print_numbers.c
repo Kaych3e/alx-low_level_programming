d1efbcd52844c3421ba96215933251554f64ac00
Kaych3e <nkaduba@gmail.com>
2022-10-14 14:37:28 -0700

    Print base number to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit number of base 10.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
