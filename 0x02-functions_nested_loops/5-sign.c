a4ed66317190d7ac28bd0f81285576c2828e093c
Kaych3e <nkaduba@gmail.com>
2022-10-18 15:26:04 -0700

    Check n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 /**
  * print_sign - Prints the sign of a number.
  * @n: number to be checked for.
+ *
  * Return: 1 if n greater than zero, 0 if n is zero, -1 if n less than zero.
  */
 
@@ -22,7 +23,7 @@ int print_sign(int n)
 			if (n < 0)
 			{
 				_putchar('-');
-	return (-1);
+				return (-1);
 			}
 }
 
