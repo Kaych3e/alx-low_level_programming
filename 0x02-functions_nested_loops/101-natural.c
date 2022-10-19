67d0452b559b846abb7e09c512df85b3ec7033e6
Kaych3e <nkaduba@gmail.com>
2022-10-18 17:30:31 -0700

    multiples of n

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints sum of multiples of 3 or 5 below 1024.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a, b;
+
+		for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
