245039b3c08e04a0935f00988dfa304e96f06015
root <nkaduba@gmail.com>
2023-05-09 03:55:32 -0700

    sort algos

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,7 +4,7 @@
 
 /**
  * binary_search - searches for a value in a sorted array of integers
- * 		using the Binary search algorithm
+ *		using the Binary search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
@@ -13,9 +13,9 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, middle, high = size -1;
+	size_t low = 0, middle, high = size - 1;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
 	while (low != high)
